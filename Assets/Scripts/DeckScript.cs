cd4679fe5a43862d587cb3abe17f4b5639dc5059
Horace <your_email@example.com>
2025-11-10 04:43:59 -0800

    Fixed shuffle bug sometime it shuffle back to the deck instead of showing on the board

--- Assets/Scripts/DeckScript.cs
+++ Assets/Scripts/DeckScript.cs
@@ -36,7 +36,7 @@ public class DeckScript : MonoBehaviour
     {
         for (int i = cardSprites.Length - 1; i > 0; i--)
         {
-            int j = Mathf.FloorToInt(Random.Range(0.0f, 1.0f) * cardSprites.Length - 1) + 1;
+            int j = Mathf.FloorToInt(Random.Range(0.0f, 1.0f) * (cardSprites.Length - 1)) + 1;
 
             Sprite face = cardSprites[i];
             cardSprites[i] = cardSprites[j];
